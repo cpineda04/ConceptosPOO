7d6f8ff0d376b04a516b56f86aae2652f3c7fb65
Cesar Pineda <cpineda@elcatador.com>
2022-10-10 20:42:58 -0400

    Finish date class

---
+++ POOConcepts/POOConcepts/DaysException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace POOConcepts
+{
+    [Serializable]
+    internal class DaysException : Exception
+    {
+        public DaysException()
+        {
+        }
+
+        public DaysException(string? message) : base(message)
+        {
+        }
+
+        public DaysException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DaysException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using POOConcepts;
+
+Console.WriteLine("Conceptos POO");
+Console.WriteLine("================");
+try
+{
+    Console.WriteLine(new Date(2022, 10, 8));
+    Console.WriteLine(new Date(2012, 1, 8));
+    Console.WriteLine(new Date(2001, 14, 19));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
