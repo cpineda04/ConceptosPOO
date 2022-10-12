af2d08ef358c3f0a5acaa0d9e66241d87f5b6720
Cesar Pineda <cpineda@elcatador.com>
2022-10-12 18:33:17 -0400

    Finish POOConcepts

--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -28,7 +28,7 @@ Employee employee1 = new SalaryEmployee
     Salary = 21500.05M
 };
 
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
 
 Employee employee2 = new CommissionEmployee
 {
@@ -43,7 +43,7 @@ Employee employee2 = new CommissionEmployee
 
 };
 
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
 
 Employee employee3 = new HourlyEmployee
 {
@@ -58,4 +58,56 @@ Employee employee3 = new HourlyEmployee
 
 };
 
-Console.WriteLine(employee3);
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommissionEmployee
+{
+    Id = 3,
+    FirstName = "Marcos",
+    LastName = "Antonio",
+    BirthDate = new Date(2005, 10, 4),
+    HiringDate = new Date(2022, 3, 13),
+    Active = true,
+    Base = 25450.45M,
+    Sales = 1200M,
+    CommissionPorcentaje = 0.50F
+
+};
+
+//Console.WriteLine(employee4);
+
+ICollection<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3, employee4
+};
+
+decimal payroll = 0;
+
+foreach (Employee employee in employees)
+{
+    Console.WriteLine(employee);
+    payroll += employee.GetValueToPay();
+}
+
+Console.WriteLine("                    =============");
+Console.WriteLine($"Total           {$"{payroll:C2}",15}");
+
+
+Invoice invoice1 = new Invoice()
+{
+    Id = 1,
+    Description = ("Iphone 11 Pro Max"),
+   Quantity = 22,
+    Price = 30200M,
+};
+
+Invoice invoice2 = new Invoice()
+{
+    Id = 2,
+    Description = ("Apple Watch Serie 5"),
+    Quantity = 10,
+    Price = 15500M,
+};
+
+Console.WriteLine(invoice1);
+Console.WriteLine(invoice2);
